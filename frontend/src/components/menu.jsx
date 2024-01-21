37f27a725215d3a1d9cea67b59437bbdcfe9f1d0
sammipau <77289757+sammipau@users.noreply.github.com>
2024-01-21 07:16:21 -0800

    nav formating (#13)

--- frontend/src/components/menu.jsx
+++ frontend/src/components/menu.jsx
@@ -1,80 +1,94 @@
-
+import { input } from "bootstrap";
 import "../styles/menu.css";
 
 export default function Menu() {
   return (
     <div className="left-menu">
-      <header className="header">
-        <div className="title">
-          <div className="text-wrapper">Navigate</div>
-          <div className="div">x</div>
+      <div className="head">
+        Navigate
+      </div>
+
+      <div class="container mt-2">
+        <div class="row justify-content-center">
+          <div class="input-group mb-3 sbar">
+            <input type="text" class="form-control rounded-pill" placeholder="Search..." aria-label="Search" aria-describedby="basic-addon2" />
+          </div>
         </div>
-        <div className="search-bar">
-          <div className="text-wrapper-2">Search...</div>
+      </div>
+
+      <hr class="white-line"></hr>
+
+      <div class="fields container mt-2">
+        <div class="form-group">
+          <input type="date" class="form-control" id="date" />
         </div>
-      </header>
-      <div className="divider" />
+      </div>
+      <div class="fields container mt-2">
+        <div class="form-group">
+          <input type="string" class="form-control" id="date" placeholder="Start to end" />
+        </div>
+      </div>
+      <div class="fields container mt-2">
+        <div class="form-group">
+          <input type="string" class="form-control" id="Occupancy" placeholder="Occupancy" />
+        </div>
+      </div>
+
+      <div id="equitment" className="equipment col-md-6">
+        <p>Equipment</p>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="Whiteboard" />
+          <label class="form-check-label" for="Whiteboard">
+            Whiteboard
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="Projector" />
+          <label class="form-check-label" for="Projector">
+            Projector
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="Mics" />
+          <label class="form-check-label" for="Mics">
+            Mics/speakers
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="Power" />
+          <label class="form-check-label" for="Power">
+            Power Outlet
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="Seats" />
+          <label class="form-check-label" for="Seats">
+            Extra chair/seating
+          </label>
+        </div>
+      </div>
+
+
+      {/* <div className="divider" />
       <div className="clip-frame">
         <div className="selectors">
           <div className="filter-section">
             <div className="filter-container">
               <div className="filters">
-                <div className="filter">
-                  <div className="text-layout">
-                    <div className="text-wrapper-3">Date</div>
-                    <div className="text-wrapper-4">--/--/--</div>
-                  </div>
-                </div>
-                <div className="filter">
-                  <div className="text-layout">
-                    <div className="text-wrapper-3">Start to End</div>
-                    <div className="text-wrapper-4">-</div>
-                  </div>
-                </div>
-                <div className="filter">
-                  <div className="text-layout">
-                    <div className="text-wrapper-3">Occupancy</div>
-                    <div className="text-wrapper-4">-</div>
-                  </div>
-                </div>
               </div>
+
             </div>
-            <div className="div-2">
-              <div className="text-wrapper-5">Equipment</div>
-              <div className="div-2">
-                <div className="checkbox">
-                  <div className="box" />
-                  <div className="text-wrapper-6">Whiteboard</div>
-                </div>
-                <div className="checkbox">
-                  <div className="box" />
-                  <div className="text-wrapper-6">Projector</div>
-                </div>
-                <div className="checkbox">
-                  <div className="box" />
-                  <div className="text-wrapper-6">Mic/speakers</div>
-                </div>
-                <div className="checkbox">
-                  <div className="box" />
-                  <div className="text-wrapper-6">Power Outlet</div>
-                </div>
-                <div className="checkbox">
-                  <div className="box" />
-                  <div className="text-wrapper-6">Extra Chairs/seating</div>
-                </div>
-              </div>
+            <div className="filter-buttons">
+              <button className="button">
+                <div className="text-wrapper-7">Apply filters</div>
+              </button>
+              <button className="div-wrapper">
+                <div className="text-wrapper-7">Reset filters</div>
+              </button>
             </div>
-          </div>
-          <div className="filter-buttons">
-            <button className="button">
-              <div className="text-wrapper-7">Apply filters</div>
-            </button>
-            <button className="div-wrapper">
-              <div className="text-wrapper-7">Reset filters</div>
-            </button>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 
