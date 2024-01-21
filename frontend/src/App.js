8f82f57aad80cc995c6e6aad1fe7fad3b23b2d86
Aryan-B <baryan.edu@gmail.com>
2024-01-20 18:46:50 -0800

    mappedin sdk complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,25 +8,29 @@ import Stat from "./components/stat";
 
 import "./App.css";
 
-import Login from './components/Login';
-import Layout from './components/Layout';
-import Missing from './components/Missing';
-import Unauthorized from './components/Unauthorized';
-import { Routes, Route } from 'react-router-dom';
+import Login from "./components/Login";
+import Layout from "./components/Layout";
+import Missing from "./components/Missing";
+import Unauthorized from "./components/Unauthorized";
+import { Routes, Route } from "react-router-dom";
 import ProtectedRoutes from "./components/protectedRoutes";
+import Landing from "./components/Landing";
 
 function App() {
   return (
-<>    
-<Routes>
-        <Route path="login" element={<Login />} />
-        <Route element={<ProtectedRoutes/>}>
-          <Route path="unauthorized" element={<Unauthorized />} />
-          <Route path="/" element={<Layout />}>
-            <Route path="*" element={<Missing />} />
-          </Route>
+    <>
+      <Routes>
+        <Route path="/" element={<Landing/>}>
+          <Route path="*" element={<Missing />} />
         </Route>
-    </Routes>
+        <Route path="home" element={<Layout />}>
+          <Route path="*" element={<Missing />} />
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="unauthorized" element={<Unauthorized />} />
+        </Route>
+      </Routes>
     </>
   );
 }
